54b4ff9264133cfd691611e2ef4488a3aba89946
Sippakorn <sippakorn.wo@kkumail.com>
2023-10-29 20:42:31 +0700

    Doing modal shop to see detail of user

--- quickhire-main/Frontend/myapp/src/pages/Mailbox/modal.js
+++ quickhire-main/Frontend/myapp/src/pages/Mailbox/modal.js
@@ -1,17 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Modal from "react-modal";
 import SwapImage from "../Shop/SwapImage";
+import CryptoJS from "crypto-js";
 
 Modal.setAppElement("#root"); // Set the root element for accessibility
 
 const Mymodal = ({ isOpen, onClose, userData }) => {
+  console.log(userData);
+  const deCryptData = (data) => {
+    return CryptoJS.AES.decrypt(data, process.env.REACT_APP_ENCRYPT_KEY).toString(CryptoJS.enc.Utf8);
+  };
+
   return (
-    <Modal isOpen={isOpen} onRequestClose={onClose} contentLabel="Example Modal" shouldCloseOnOverlayClick={false} className="backdrop">
-      <div className="container w-5/6 h-5/6 md:h-2/3 rounded-md bg-white flex flex-col justify-between">
+    <Modal isOpen={isOpen} onRequestClose={onClose} contentLabel="Example Modal" shouldCloseOnOverlayClick={false} className="flex items-center justify-center h-screen rounded-md ">
+      <div className="container w-5/6 rounded-md bg-white flex flex-col justify-center m-auto shadow-lg">
         <div className="flex flex-col p-5">
           {userData.img !== undefined && <SwapImage images={JSON.parse(userData.img)} />}
-          <h2>{userData.fullname}</h2>
-          <p>{userData.email}</p>
+          <div className="flex justify-center text-lg font-bold mb-3">{deCryptData(userData.fullname)}</div>
+          <div className="grid grid-cols-2 gap-6">
+            <div>ชื่อเล่น : {deCryptData(userData.nickname)}</div>
+            <div>เพศ : {deCryptData(userData.sex)}</div>
+          </div>
+          <div>Email : {userData.email}</div>
+          <div>เบอร์โทรศัพท์ : {deCryptData(userData.telnumber)}</div>
+          <div>วันเกิด : {deCryptData(userData.birthdate)}</div>
+          <div>สัญชาติ : {deCryptData(userData.national)}</div>
+          <div>ความสามารถพิเศษ : {deCryptData(userData.talent)}</div>
+          <div>การศึกษา : {deCryptData(userData.degree)}</div>
+          <div>ระดับภาษาไทย : {deCryptData(userData.thailevel)}</div>
+          <div>ระดับภาษาอังกฤษ : {deCryptData(userData.englevel)}</div>
+          <div>ทักษาการขับรถ : {deCryptData(userData.vehicle)}</div>
+          <div>ประสบการณ์ทำงาน : {deCryptData(userData.workexp)}</div>
         </div>
         <div className="flex flex-col">
           <button type="button" onClick={onClose} class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-bold rounded-b-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
@@ -20,6 +39,7 @@ const Mymodal = ({ isOpen, onClose, userData }) => {
         </div>
       </div>
     </Modal>
+
   );
 };
 
