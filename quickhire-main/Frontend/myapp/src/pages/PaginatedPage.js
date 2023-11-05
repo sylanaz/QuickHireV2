6c6f9319025083ed6604862ea3710e3e5fdf18d9
Sippakorn <sippakorn.wo@kkumail.com>
2023-11-05 22:44:22 +0700

    Add style at paginate when it current page

--- quickhire-main/Frontend/myapp/src/pages/PaginatedPage.js
+++ quickhire-main/Frontend/myapp/src/pages/PaginatedPage.js
@@ -3,11 +3,12 @@ import React from "react";
 const PaginatedPage = ({ totalPages, currentPage, onPageChange }) => {
 
   const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
+
   return (
     <div className="flex">
       <span className="mx-auto font-semibold md:text-2xl">Page :</span>
       {pageNumbers.map((page) => (
-        <button key={page} onClick={() => onPageChange(page)} className="mx-2 font-semibold md:text-2xl">
+        <button key={page} onClick={() => onPageChange(page)} className={`mx-2 font-semibold md:text-2xl ${currentPage == page && `text-red-600`}`}>
           {page}
         </button>
       ))}
